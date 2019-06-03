98904d706ae8949139e5f35720c40c49fb72bedc
antoniobarbalau <antoniobarbalau@gmail.com>
2019-06-03 14:17:40 +0300

    tonio

--- plugin/create_standalone_section.py
+++ plugin/create_standalone_section.py
@@ -4,16 +4,17 @@ import subprocess
 
 
 section_text = open(sys.argv[1], 'r').read()
-template_text = open('/home/tonio/.config/nvim/bundle/vim_tonio_latex/plugin/latex_template.tex', 'r').readlines()
-# template_text = open('./latex_template.tex', 'r').readlines()
+template_text = open(
+    '/home/tonio/.config/nvim/bundle/vim_tonio_latex/' +
+    'plugin/satanism_template.tex', 'r'
+).readlines()
 
 output_text = ''
 
-
 copy_flag = True
 for line in template_text:
-    if 'center' in line:
-        copy_flag = False
+    # if 'center' in line:
+    #     copy_flag = False
 
     if copy_flag:
         if '<--**-->' in line:
@@ -21,17 +22,19 @@ for line in template_text:
         else:
             output_text += line
 
-    if 'vspace' in line:
-        copy_flag = True
+    # if 'vspace' in line:
+    #     copy_flag = True
 
 # print(output_text)
 
 filename = os.path.dirname(sys.argv[1]) + '/Standalone_' + os.path.basename(sys.argv[1])
-print(filename)
+# print(filename)
 
 with open(filename, 'w') as f:
     f.write(output_text)
-subprocess.call(['pdflatex', filename, '-output-directory', os.path.dirname(sys.argv[1])])
+subprocess.call([
+    'pdflatex', filename, '-output-directory', os.path.dirname(sys.argv[1])
+])
 # subprocess.call(['zathura', filename.replace('tex', 'pdf')])
 
 
